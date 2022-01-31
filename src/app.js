f46954b09b2108ea41580fcfd35323a5d569d9d4
lombable <rinconaedgar@gmail.com>
2022-01-31 11:21:12 -0300

    done

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let who = ["My dog", "Rigo", "Pikachu", "Voldemort"];
+  let action = [" cried", " burned", " stole", " broke"];
+  let what = [" my shorts", " my food", " my homework", " my hair"];
+  let when = [" last week", " this morning", " yesterday noon"];
+
+  let who1 = Math.floor(Math.random() * (who.length - 1) + 1);
+  let action1 = Math.floor(Math.random() * (action.length - 1) + 1);
+  let what1 = Math.floor(Math.random() * (what.length - 1) + 1);
+  let when1 = Math.floor(Math.random() * (when.length - 1) + 1);
+  return who[who1] + action[action1] + what[what1] + when[when1];
 };
